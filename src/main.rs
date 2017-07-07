d7b203c7ea655c6b4c29f9d309e50f9710c39e40
Kevin Rosendahl <kevindrosendahl@Kevins-MacBook-Pro.local>
2017-07-06 20:23:38 -0700

    chroot then print root

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+
+extern crate nix;
+
+fn print_root() -> std::io::Result<()> {
+    for entry in fs::read_dir("/")? {
+        let entry = entry?;
+        let file_name = entry.file_name();
+        println!("{:?}", file_name);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_root().unwrap();
+    nix::unistd::chroot("rootfs");
+    print_root().unwrap();
 }
